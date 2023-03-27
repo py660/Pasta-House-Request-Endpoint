df5298ab9886b54859876eb558cbf1b108fa1034
Jason <raspberrypy660@gmail.com>
2023-03-27 19:49:45 -0400

    DEV

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.sendFile("./index.html");
-})
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
