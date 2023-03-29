e672afca2cce9a6adc1c1dc7a21eebbaf46c7aad
Jason <raspberrypy660@gmail.com>
2023-03-29 11:59:06 -0400

    Stuff

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit', function (req, res) {
+  res.render('the_template', { name: req.body.name });
 });
 
 app.listen(port, () => {
