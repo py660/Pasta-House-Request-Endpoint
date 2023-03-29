74fc0e4dc68f23a30732f5d5d0a8c8a9de1cb8c2
Jason <raspberrypy660@gmail.com>
2023-03-29 18:08:12 -0400

    DEV
    Signed-off-by: Jason <raspberrypy660@gmail.com>

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const port = 3000
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.engine('html', require('ejs').renderFile);
+app.use(express.static('assets'));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/demo', function(req, res){
+  res.sendFile(__dirname + '/template.html');
+});
+
 app.post('/submit', function (req, res) {
-  res.render('the_template', { name: req.body.name });
+  console.log(req.body)
+  res.render('index.html', { data: JSON.stringify(req.body) });
 });
 
 app.listen(port, () => {
