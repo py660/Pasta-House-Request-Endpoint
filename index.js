425af5fc974374d2442f7960096dcc3ac05e6a57
Jason <raspberrypy660@gmail.com>
2023-03-31 10:14:29 -0400

    DEV

--- index.js
+++ index.js
@@ -1,5 +1,49 @@
+(async() => {
+
 const express = require('express');
-const fs = require('fs');
+let   request = require('request');
+const j = request.jar();
+//console.log(process.env.PW);
+const cookie = request.cookie('PW=' + process.env.PW);
+const url = 'https://orange-red-rooster-coat.cyclic.app';
+j.setCookie(cookie, url);
+
+//console.log(j);
+
+function write(filename, contents){
+  return new Promise((resolve, reject) => {
+    filename = filename || "tmp"
+    request.put('https://orange-red-rooster-coat.cyclic.app/' + filename, {jar: j, headers: {'content-type' : 'application/json'}, body: contents, json: true}, (err, res, body) => {
+      if (err) { console.log(err); reject(err); }
+      //console.log(body)
+      if (body == "ok"){
+        resolve(body);
+      }
+      else{
+        reject(body);
+      }
+    });
+  })
+}
+
+function read(filename){
+  return new Promise((resolve, reject) => {
+    filename = filename || "tmp"
+    request.get('https://orange-red-rooster-coat.cyclic.app/' + filename, {jar: j, json: true}, (err, res, body) => {
+      if (err) { console.log(err); reject(err); }
+      //console.log(body)
+      if (res.statusCode != 200) {
+        reject('Invalid status code: ' + response.statusCode);
+      }
+      resolve(body)
+    });
+  });
+}
+
+//console.log(await write("db.json", {"bookmarklets": []}))
+//console.log(await read("db.json"))
+
+//const fs = require('fs');
 //const fs = require('@cyclic.sh/s3fs') 
 const app = express();
 const port = 3000;
@@ -8,29 +52,22 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.engine('html', require('ejs').renderFile);
 app.use(express.static(__dirname + '/assets'));
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/demo', function(req, res){
-  res.sendFile(__dirname + '/template.html');
-});
-
-/*app.get('/db.json', function(req, res){
+app.get('/admin', async function(req, res){
   console.log(req.cookies);
-  res.sendFile(__dirname + '/db.json');
-})
-
-app.post('/db.json', function(req, res){
-  console.log(req.cookies);
-  res.sendFile(__dirname + "/db.json")
-})*/
-
-app.get('/admin', function(req, res){
-  console.log(req.cookies);
-  let existing = JSON.parse(fs.readFileSync("db.json"));
-  res.render('admin.html', { data: existing })
+  if (req.cookies.PW == process.env.PW){
+    let existing = await read("db.json");
+    res.render('admin.html', { data: existing });
+  }
+  else{
+    res.send("<h1>403 Forbidden</h1><p>Shoo, get out of here.</p>")
+  }
 })
 
 function trim(yourString, maxLength){ // Define trim function
@@ -49,7 +86,8 @@ function trim(yourString, maxLength){ // Define trim function
   }
 }
 
-app.post('/submit', function(req, res){
+app.post('/submit', async function(req, res){
+  
   console.log(req.body);
   let body = req.body;
   cleaned = {
@@ -63,17 +101,27 @@ app.post('/submit', function(req, res){
   console.log(cleaned);
   console.log("-" * 70);
   res.render('index.html', { data: cleaned });
-  let existing = JSON.parse(fs.readFileSync("db.json"));
+  let existing = await read("db.json");
+  //let existing = JSON.parse(fs.readFileSync("db.json"));
   console.log(existing.bookmarklets);
   existing.bookmarklets.push(cleaned);
   console.log(existing.bookmarklets);
-  fs.writeFileSync("db.json", JSON.stringify(existing, 0, 4));
+
+  write("db.json", existing);
+  //fs.writeFileSync("db.json", JSON.stringify(existing, 0, 4));
 });
 
 app.get('/submit', function(req, res){
   res.redirect('/');
 });
 
+app.get('/after', function(req, res){
+  res.redirect('/');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+})();
